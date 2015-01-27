e58fdd774d3b72fbd2e42c642b414fdd18f81bae
filmdate <filmdateee@gmail.com>
2015-01-27 19:55:51 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+//PREPARACIÓN ===============================================================
+
+//Definición de las dependencias
+var express  = require('express');
+var request = require('request');
+
+var app      = express(); 
+var port     = process.env.PORT || 3000;
+
+
+// Rutas
+app.get('/cartelera', function(req, res) {
+
+	var url='http://filmdate-filmdate.rhcloud.com/api/api.php/getCartelera';
+
+	var request = require('request');
+
+	request({url:url, json:"true"}, function (error, response, body) {
+
+		if (!error && response.statusCode == 200) {
+
+			console.log(body);
+			res.json(body);
+		}
+		else {
+
+			res.json({error:"request error"});
+		}
+	});
+
+});
+
+// ARRANQUE DEL SERVIDOR ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
