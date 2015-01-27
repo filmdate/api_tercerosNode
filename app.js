aaa6afccca5e39291c8cff6cbe3de371f7bd3a84
filmdate <filmdateee@gmail.com>
2015-01-27 20:45:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,30 @@ var port     = process.env.PORT || 3000;
 
 
 // Rutas
-app.get('/cartelera', function(req, res) {
+app.get("/cartelera", function(req, res) {
 
 	var url='http://filmdate-filmdate.rhcloud.com/api/api.php/getCartelera';
+
+	var request = require('request');
+
+	request({url:url, json:"true"}, function (error, response, body) {
+
+		if (!error && response.statusCode == 200) {
+
+			console.log(body);
+			res.json(body);
+		}
+		else {
+
+			res.json({error:"request error"});
+		}
+	});
+
+});
+
+app.get("/pelicula/:titulo", function(req, res) {
+
+	var url='http://filmdate-filmdate.rhcloud.com/api/api.php/getPelicula/'+req.params.titulo;
 
 	var request = require('request');
 
